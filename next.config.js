bfdc1174707b3ffe9b7c7ac56ebbb683f1da1641
Andrew Ortiz <werdnajoc@gmail.com>
2019-12-19 22:32:19 -0300

    deplot

--- next.config.js
+++ next.config.js
@@ -1,4 +1,5 @@
 const withCSS = require('@zeit/next-css')
 module.exports = withCSS({
     /* config options here */
+    assetPrefix: process.env.NODE_ENV === 'production' ? '/cookunity-themaze.github.io' : '',
 });
